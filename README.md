65cea4dfb2456dac2beb24cf59f23014a60765d4
Toby <toby.monk@teamgoldflame.com>
2021-12-14 13:33:09 +0100

    Working version of Lunch Checker app

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# angularjs-pdbgnz
+# Angular JS test project 1 - Lunch checker
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angularjs-pdbgnz)
+This lunch checker is based on the Coursera course Single page web app development (John Hopkins university)
+
+
+*App purpose*
+The app parses a string input and changes the output depending on the number of items separated by commas
+
+*Demonstrates:*
+- Angular binding to input box via ng-model
+- Angular binding to button via ng-click which calls a function on $scope < parseList()
+- Angular binding to output via {{ xxxx }}
+
+
+
+To run browser sync to load the files etc:
+
+1. open / run termiunus in sublime from the control palette cmd + shift + P
+2. run browser-sync start --server --directory --files "**/*"   
--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 (function () {
   'use strict';
 
-  angular.module('MsgApp', []).controller('MsgController', MsgController);
+  angular.module('LunchApp', [])
+    .controller('AppController', MsgController);
 
   MsgController.$inject = ['$scope'];
+
   function MsgController($scope) {
-    $scope.name = 'Toby';
+    $scope.lunchMessage = 'Please enter data first';
+    $scope.numItems = 0;
+
+
+    $scope.parseList = function() {
+      var numItems = 0; //get the number of lunch items
+
+      numItems = countItems($scope.inputList);
+
+      $scope.numItems = numItems;
+
+      $scope.lunchMessage =  ((numItems == 0) ? 
+        'Please enter data first.' : 
+          ((numItems > 3) ? 'Too much!' : 'Enjoy!')
+        );
+
+    };
+
+    function countItems(string){
+      // Counts the number of non-zero items in a list separated by commas
+      // Ignores items which are either spaces or undefined
+
+      var itemCount = 0;
+      var items = [];
+      var itemLengths = [];
+
+      if (typeof string !== 'undefined') {
+      
+        //Split string based on commas, remove white spaces
+        items = string.trim().split(',');
+
+        //count the length of each item in the list, after trimming for whitespace
+        itemLengths = items.map(function(phrase){
+          return phrase.trim().length;
+        })
+
+        console.log(itemLengths);
+        console.log(itemLengths.filter(item => item > 0));
+
+        itemCount = itemLengths.filter(item => item > 0).length;
+
+      }
+
+      return itemCount;
+    }
+
+
   }
+
 })();
